c724a8866f6110af272256f765b587b66f85e02d
theGUMP679 <78369496+theGUMP679@users.noreply.github.com>
2021-02-22 16:01:16 -0500

    stuff

---
+++ Assets/Scripts/MeetPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MeetPlayer : MonoBehaviour
+{
+
+    public InputField playerInput;
+    public InputField playerAge;
+    public static string playerName;
+    public int myAge;
+
+
+    public void SetName ()
+    {
+        playerName = playerInput.text;
+        myAge = int.Parse(playerAge.text);
+        Debug.Log("Name and Age of player is" + playerName + myAge);
+    }
+
+  
+    public void StartGame()
+    {
+        PlayerPrefs.SetString("ThisPlayer", playerName);
+        PlayerPrefs.SetInt("ThisAge", myAge);
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class UIController : MonoBehaviour
+{
+
+    public string playerName2;
+    public int playerLegal;
+
+    public Text tellPlayer;
+    public Slider mooseColor;
+    public Slider mooseSize;
+    public Slider mooseRotate;
+    public GameObject Moose;
+
+
+    
+
+    public void changeMooseColor()
+    {
+        float newMooseColor = mooseColor.value;
+        Moose.GetComponent<SpriteRenderer>().color = Colors[(int)newMooseColor];
+
+        Colors = new Color32[4];
+        Colors[0] = new Color32(185, 0, 185, 255);
+        Colors[1] = new Color32(0, 127, 255, 255);
+        Colors[2] = new Color32(0, 0, 255, 255);
+        Colors[3] = new Color32(0, 255, 255, 255);
+    }
+
+
+    public void changeMooseSize()
+    {
+        float newMooseSize = mooseSize.value;
+        Moose.transform.localScale = new Vector2(newMooseSize, newMooseSize);
+    }
+
+    Color32[] Colors;
+
+}
